16a39f63260d16148ce4a7f3433f76dc8d3e5069
Rahul sha <newu2040@gmail.com>
2026-05-27 18:05:28 +0545

    J

--- app/src/androidTest/java/com/example/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
   fun useAppContext() {
     // Context of the app under test.
     val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-    assertEquals("com.aistudio.chargewise.xtclzp", appContext.packageName)
+    assertEquals("com.aistudio.chargewise.bvmqpy", appContext.packageName)
   }
 }
